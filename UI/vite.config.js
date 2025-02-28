a691a15ef0d6ea9fc0e99718448c643b3a458aaf
elijah571 <elijahfx43@gmail.com>
2025-02-28 15:20:32 +0100

    change the .env file

--- UI/vite.config.js
+++ UI/vite.config.js
@@ -1,24 +1,9 @@
-// vite.config.js
-import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  return {
-    plugins: [react()],
-    server: {
-      host: "0.0.0.0",
-      proxy: mode === "development" ? {
-        "/api": {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          secure: false,
-        }
-      } : undefined,
-    },
-    preview: {
-      host: "0.0.0.0",
-      port: 4173,
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': {}
+  }
 });
